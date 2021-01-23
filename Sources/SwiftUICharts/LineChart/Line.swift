8d368ab257835b98a2259084588934f6ca01a562
Anthony <me@anthony-dacruz.com>
2021-01-23 18:08:42 +0100

    New SimpleLineView to customize appearance

--- Sources/SwiftUICharts/LineChart/Line.swift
+++ Sources/SwiftUICharts/LineChart/Line.swift
@@ -90,6 +90,10 @@ public struct Line: View {
         }
     }
     
+//    public init(data: ChartData, frame: CGRect, touchLocation: CGPoint, showIndicator: Bool ) {
+//
+//    }
+    
     func getClosestPointOnPath(touchLocation: CGPoint) -> CGPoint {
         let closest = self.path.point(to: touchLocation.x)
         return closest
@@ -100,7 +104,12 @@ public struct Line: View {
 struct Line_Previews: PreviewProvider {
     static var previews: some View {
         GeometryReader{ geometry in
-            Line(data: ChartData(points: [12,-230,10,54]), frame: .constant(geometry.frame(in: .local)), touchLocation: .constant(CGPoint(x: 100, y: 12)), showIndicator: .constant(true), minDataValue: .constant(nil), maxDataValue: .constant(nil))
-        }.frame(width: 320, height: 160)
+            Line(data: ChartData(points: [12,-230,10,54]),
+                 frame: .constant(geometry.frame(in: .local)),
+                 touchLocation: .constant(CGPoint(x: 100, y: 12)),
+                 showIndicator: .constant(false),
+                 minDataValue: .constant(0),
+                 maxDataValue: .constant(60))
+        }.frame(width: 120, height: 160)
     }
 }
--- Sources/SwiftUICharts/LineChart/SimpleLineView.swift
+++ Sources/SwiftUICharts/LineChart/SimpleLineView.swift
@@ -8,10 +8,8 @@
 
 import SwiftUI
 
-public struct LineView: View {
+public struct SimpleLineView: View {
     @ObservedObject var data: ChartData
-    public var title: String?
-    public var legend: String?
     public var style: ChartStyle
     public var darkModeStyle: ChartStyle
     public var valueSpecifier:String
@@ -25,15 +23,18 @@ public struct LineView: View {
     @State private var currentDataNumber: Double = 0
     @State private var hideHorizontalLines: Bool = false
     
+    @State var minDataValue: Double? = nil
+    @State var maxDataValue: Double? = nil
+    
     public init(data: [Double],
                 title: String? = nil,
                 legend: String? = nil,
                 style: ChartStyle = Styles.lineChartStyleOne,
-                valueSpecifier: String? = "%.1f") {
-        
+                valueSpecifier: String? = "%.1f",
+                minDataValue: Double? = nil,
+                maxDataValue: Double? = nil
+                ) {
         self.data = ChartData(points: data)
-        self.title = title
-        self.legend = legend
         self.style = style
         self.valueSpecifier = valueSpecifier!
         self.darkModeStyle = style.darkModeStyle != nil ? style.darkModeStyle! : Styles.lineViewDarkMode
@@ -42,38 +43,20 @@ public struct LineView: View {
     public var body: some View {
         GeometryReader{ geometry in
             VStack(alignment: .leading, spacing: 8) {
-                Group{
-                    if (self.title != nil){
-                        Text(self.title!)
-                            .font(.title)
-                            .bold().foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
-                    }
-                    if (self.legend != nil){
-                        Text(self.legend!)
-                            .font(.callout)
-                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor : self.style.legendTextColor)
-                    }
-                }.offset(x: 0, y: 20)
                 ZStack{
                     GeometryReader{ reader in
                         Rectangle()
                             .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.backgroundColor : self.style.backgroundColor)
-                        if(self.showLegend){
-                            Legend(data: self.data,
-                                   frame: .constant(reader.frame(in: .local)), hideHorizontalLines: self.$hideHorizontalLines)
-                                .transition(.opacity)
-                                .animation(Animation.easeOut(duration: 1).delay(1))
-                        }
                         Line(data: self.data,
-                             frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width - 30, height: reader.frame(in: .local).height)),
+                             frame: .constant(CGRect(x: 0, y: 0, width: reader.frame(in: .local).width, height: reader.frame(in: .local).height)),
                              touchLocation: self.$indicatorLocation,
                              showIndicator: self.$hideHorizontalLines,
-                             minDataValue: .constant(nil),
-                             maxDataValue: .constant(nil),
+                             minDataValue: minDataValue,
+                             maxDataValue: maxDataValue,
                              showBackground: true,
                              gradient: self.style.gradientColor
                         )
-                        .offset(x: 30, y: -20)
+                        .offset(x: 0, y: 0)
                         .onAppear(){
                             self.showLegend = false
                         }
@@ -91,9 +74,9 @@ public struct LineView: View {
                 .gesture(DragGesture()
                 .onChanged({ value in
                     self.dragLocation = value.location
-                    self.indicatorLocation = CGPoint(x: max(value.location.x-30,0), y: 32)
+                    self.indicatorLocation = CGPoint(x: max(value.location.x,0), y: 32)
                     self.opacity = 1
-                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width-30, height: 240)
+                    self.closestPoint = self.getClosestDataPoint(toPoint: value.location, width: geometry.frame(in: .local).size.width, height: 240)
                     self.hideHorizontalLines = true
                 })
                     .onEnded({ value in
@@ -119,7 +102,7 @@ public struct LineView: View {
     }
 }
 
-struct LineView_Previews: PreviewProvider {
+struct SimpleLineView_Previews: PreviewProvider {
     
     var chartStyle = ChartStyle(backgroundColor: Color.white, accentColor: Color.blue, gradientColor: GradientColor(start: Color.red, end: Color.green), textColor: Color.black, legendTextColor: Color.white, dropShadowColor: Color.black.opacity(0))
     
@@ -127,9 +110,9 @@ struct LineView_Previews: PreviewProvider {
     
     static var previews: some View {
         Group {
-            LineView(data: [8,23,54,32,12,37,7,23,43], title: "Full chart", style: ChartStyle(backgroundColor: Color.white, accentColor: Color.blue, secondGradientColor: Color.green, textColor: Color.black, legendTextColor: Color.white, dropShadowColor: Color.black.opacity(0)))
+            SimpleLineView(data: [20,20,20,20,23], style: ChartStyle(backgroundColor: Color.white, accentColor: Color.blue, secondGradientColor: Color.green, textColor: Color.black, legendTextColor: Color.white, dropShadowColor: Color.black.opacity(0)))
             
-            LineView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles.lineChartStyleOne)
+            SimpleLineView(data: [282.502, 284.495, 283.51, 285.019, 285.197, 286.118, 288.737, 288.455, 289.391, 287.691, 285.878, 286.46, 286.252, 284.652, 284.129, 284.188], title: "Full chart", style: Styles.lineChartStyleOne)
             
         }
     }
